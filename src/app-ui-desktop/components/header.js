c6849f3485f78ab34dad068e0fa9d6aef708116e
James Cavanagh <james.cavanagh@oneadvanced.com>
2022-10-23 22:33:12 +0100

    feat: add nav component

--- src/app-ui-desktop/components/header.js
+++ src/app-ui-desktop/components/header.js
@@ -3,7 +3,37 @@ export function createHeader (helpers) {
   return {
     renderer,
     render () {
-      return html`<header>Desktop header</header>`
+      return html`
+        <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+          <div class="relative flex grid items-center grid-cols-2 lg:grid-cols-3">
+            <nav>
+              <ul>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#" role="button">Button</a></li>
+              </ul>
+            </nav>
+            
+            <a href="/" aria-label="Company" title="Company" class="inline-flex items-center lg:mx-auto">
+              <img src="/assets/img/logo.svg" alt="" style="height: 64px;">
+              <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">FicusJS</span>
+            </a>
+            <ul class="flex items-center hidden ml-auto space-x-8 lg:flex">
+              <li><a href="/" aria-label="Sign in" title="Sign in" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Sign in</a></li>
+              <li>
+                <a
+                  href="/"
+                  class="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                  aria-label="Sign up"
+                  title="Sign up"
+                >
+                  Sign up
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      `
     }
   }
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,12 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: 'rgb(var(--color-primary) / <alpha-value>)',
+        secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+      }
+    }
   },
   plugins: []
 }
