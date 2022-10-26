c86f75b10a10f7e57f593b91617576410bda3743
James Cavanagh <james.cavanagh@oneadvanced.com>
2022-10-26 21:30:44 +0100

    feat: add native nav to header template

--- src/app-ui-desktop/components/header.js
+++ src/app-ui-desktop/components/header.js
@@ -5,54 +5,40 @@ export function createHeader (helpers) {
     render () {
       return html`
         <header class="relative overflow-hidden">
-          <!-- Top navigation -->
           <nav aria-label="Top" class="bg-white bg-opacity-90 backdrop-blur-xl backdrop-filter">
-            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div class="mx-auto max-w-7xl w-full px-8">
               <div class="flex h-16 items-center">
-                
-                <!-- Logo -->
-                <div>
-                  <a href="#">
-                    <span class="sr-only">FicusJS</span>
-                    <img class="h-8 w-auto" src="/assets/img/logo.svg" alt="Ficus logo">
-                  </a>
-                </div>
-      
-                <!-- Navigation -->
-                <div class="hidden lg:ml-8 lg:block lg:self-stretch">
-                  <div class="flex h-full space-x-8">
-                    <a href="#" class="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800">Home</a>
-                    <a href="#" class="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800">Shop</a>
-                    <a href="#" class="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800">Gallery</a>
-                    <a href="#" class="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800">Contact</a>
-                  </div>
-                </div>
 
-                
-      
-                <div class="ml-auto flex items-center">
+                <!-- Logo & navigation -->
+                <ul> 
+                  <li>
+                    <a href="#">
+                      <span class="sr-only">FicusJS</span>
+                      <img class="h-8 w-auto" src="/assets/img/logo.svg" alt="Ficus logo">
+                    </a>
+                  </li>
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">Shop</a></li>
+                  <li><a href="#">Gallery</a></li>
+                  <li><a href="#">Contact</a></li>
+                </ul>
 
-                  <!-- Account -->
-                  <div class="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
-                    <a href="#" class="text-sm font-medium text-gray-700 hover:text-gray-800">Sign in</a>
-                    <span class="h-6 w-px bg-gray-200" aria-hidden="true"></span>
-                    <a href="#" class="text-sm font-medium text-gray-700 hover:text-gray-800">Create account</a>
-                  </div>
-      
-                  <!-- Search -->
-                  <div class="flex lg:ml-6">
-                    <a href="#" class="p-2 text-gray-400 hover:text-gray-500">
+                <!-- Account, search & basket-->
+                <ul class="ml-auto">
+                  <li><a href="#">Sign in</a></li>
+                  <span class="h-6 w-px mx-2 bg-gray-200" aria-hidden="true"></span>
+                  <li><a href="#">Create account</a></li>
+                  <li>
+                    <a href="#" class="text-gray-400 hover:text-gray-500">
                       <span class="sr-only">Search</span>
                       <!-- Heroicon name: outline/magnifying-glass -->
                       <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                       </svg>
                     </a>
-                  </div>
-      
-                  <!-- Cart -->
-                  <div class="ml-4 flow-root lg:ml-6">
-                    <a href="#" class="group -m-2 flex items-center p-2">
+                  </li>
+                  <li>
+                    <a href="#" class="flex items-center">
                       <!-- Heroicon name: outline/shopping-bag -->
                       <svg class="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
@@ -60,9 +46,9 @@ export function createHeader (helpers) {
                       <span class="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">0</span>
                       <span class="sr-only">items in cart, view bag</span>
                     </a>
-                  </div>
+                  </li>
+                </ul>
 
-                </div>
               </div>
             </div>
           </nav>
